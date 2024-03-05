c2a35e770a07c65b9359c4ee14edaddc88d7c9a5
Lucca Maia <luccamairosa@gmail.com>
2024-03-04 21:47:57 -0300

    Classe SmartTv

---
+++ src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(14);
+
+        System.out.println("Tv Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual? " + smartTv.canal);
+        System.out.println("Volume Atual? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Tv Ligada? " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("Tv Ligada? " + smartTv.ligada);
+        
+    }
+}
